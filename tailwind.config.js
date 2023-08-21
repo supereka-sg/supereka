9906b6e1188e60eda6764103897e80cf5146083e
nawzaysfinah <nawzaysfinah@gmail.com>
2023-08-21 12:55:50 +0800

    reset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,9 @@
-/* eslint-env node */
 /** @type {import('tailwindcss').Config} */
-
-module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  mode: "jit",
+export default {
+  content: [],
   theme: {
-    fontFamily: {
-      sans: ["Inter", "sans-serif"],
-      serif: ["EB Garamond", "serif"],
-    },
-    extend: {
-      colors: {
-        primary: "#262626",
-        background: "#EEEEEE",
-        secondary: "#525252",
-        tertiary: "#a3a3a3",
-        tertiary2: "#d6d3d1",
-        "black-100": "#100d25",
-        "black-200": "#090325",
-        "white-100": "#f3f3f3",
-      },
-      boxShadow: {
-        card: "0px 35px 120px -15px #211e35",
-      },
-      screens: {
-        xs: "450px",
-      },
-      backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
